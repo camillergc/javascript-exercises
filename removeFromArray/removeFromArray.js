4862e758087a063575527b4abc67fbdc1ac6e3d8
camille <camillergc@gmail.com>
2021-03-26 17:25:04 -0400

    finished removeFromArray using loops instead of the filter() method. finished sumAll.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,23 +1,30 @@
-// const removeFromArray = function (array, ...theArgs) {
-//     let argsArray = Array.from(theArgs);
-//     while (argsArray.length > 0) {
-//         array = array.filter(elem => elem != argsArray.pop());
-//     }
-
-//     return array;
-
-// }
-
 const removeFromArray = function (array, ...theArgs) {
-    let argsArray = Array.of(theArgs);
-    while (argsArray.length > 0) {
-        array = array.filter(function (value, index, arr) {
-            return value != argsArray.pop();
-        });
+    let argsArray = Array.from(theArgs);
+    for (i = 0; i < argsArray.length; i++) {
+        index = array.indexOf(argsArray[i]);
+        if(index != -1){
+            for (j = index; j < array.length - 1; j++) {
+                array[j] = array[j+1];
+            }
+            array.pop();
+        }
     }
 
     return array;
 
 }
+/*couldn't get array.filter to work, but would like to know what i was doing wrong someday.
+ * 
+ */
+// const removeFromArray = function (array, ...theArgs) {
+//     let argsArray = Array.of(theArgs);
+//     while (argsArray.length > 0) {
+//         array = array.filter(function (value, index, arr) {
+//             return value != argsArray.pop();
+//         });
+//     }
+//     return array;
+
+// }
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return `ERROR`;
+    }
+    if (a < 0 || b < 0) {
+        return `ERROR`;
+    }
+    sum = 0;
+    for (i = Math.min(a, b); i <= Math.max(a, b); i++) {
+        sum += i;
+    }
+    return sum;
 
 }
 
